da7bb375037dff47495937952c1aa9aee99bce8f
Gor Chinaryan <gor.chinaryan@god.de>
2022-02-24 15:12:03 +0100

    mvvm implemented

---
+++ MVVM_WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using MVVM_WpfApp1.ViewModel;
+
+
+namespace MVVM_WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private UserViewModel model;
+
+        public MainWindow()
+        {
+            model = new UserViewModel();
+            model.Username = "Alex";
+            DataContext = model;
+
+            InitializeComponent();
+        }
+
+        private void btn_ChangeUsername_Click(object sender, RoutedEventArgs e)
+        {
+            model.Username = "Gg1-1";
+        }
+    }
+}
---
+++ MVVM_WpfApp1/ViewModel/UserViewModel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Input;
+
+namespace MVVM_WpfApp1.ViewModel
+{
+    public class UserViewModel : INotifyPropertyChanged
+    {
+        private string _username;
+
+        public string Username
+        {
+            get
+            {
+                return _username;
+            }
+            set
+            {
+                _username = value;
+                OnPropertyChanged("Username");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public UserViewModel()
+        {
+            _changeNameCommand = new DelegateCommand(OnChangeName);
+        }
+
+        //for commands: button, checkbox usw. 
+        private readonly DelegateCommand _changeNameCommand;
+
+        public ICommand ChangeNameCommand => _changeNameCommand;
+
+        private void OnChangeName(object commandParameter)
+        {
+            this.Username = "Walter";
+        }
+    }
+
+    public class DelegateCommand : ICommand
+    {
+        private readonly Action<object> _executeAction;
+
+        public DelegateCommand(Action<object> executeAction)
+        {
+            _executeAction = executeAction;
+        }
+
+        public void Execute(object parameter) => _executeAction(parameter);
+
+        public bool CanExecute(object parameter) => true;
+
+        public event EventHandler CanExecuteChanged;
+    }
+}
